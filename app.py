ce9cdfb7ac993ed202ea614c525804a9a7f45a71
d-pamneja <dpamneja@gmail.com>
2024-04-17 07:33:50 +0530

    App

--- app.py
+++ app.py
@@ -10,4 +10,19 @@ from src.invoice_extractor.logger import logging
 load_dotenv()
 genai.configure(api_key = os.getenv('GOOGLE_API_KEY'))
 
-logging.info("App Running")
+# Initialising the Streamlit Application
+
+st.set_page_config(page_title="Invoice Extractor using Gemini Vision")
+
+st.header("Gemini Application")
+input = st.text_input("Enter the Input Prompt",key="input")
+uploaded_file = st.file_uploader("Chosse the invoice..",type=["jpg","jpeg","png"])
+image = ""
+
+if uploaded_file is not None:
+    # Once the upload is done, display the image once to the user
+    image = Image.open(uploaded_file)
+    st.image(image,caption="Uploaded Image",use_column_width=True)
+    
+submit = st.button("Review the Invoice")
+
