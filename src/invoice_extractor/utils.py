b0779f1ea1ff3022f85b8747c0f2969e614f1007
d-pamneja <dpamneja@gmail.com>
2024-04-17 07:25:17 +0530

    Utils

--- src/invoice_extractor/utils.py
+++ src/invoice_extractor/utils.py
@@ -10,9 +10,32 @@ genai.configure(api_key = os.getenv('GOOGLE_API_KEY'))
 
 # Now, let us create a function to load the image and extract the text from it using Gemini Pro Vision Model
 
-def get_gemini_response(input,image,prompt):
+def get_gemini_response(input,image,prompt): 
+    # Here, we load the model and generate a response based on the given parameters of input, image and prompt as a list
     model = genai.GenerativeModel('gemini-pro-vision')
     response = model.generate_content([input,image[0],prompt])
     
     return response.text
 
+
+def input_image_setup(file):
+    # Here, we convert the image data into bytes and setup the image as a list of json object with the requried data to read the image
+    try:
+        if file is not None:
+            bytes_data = file.getvalue() # Converts the image into bytes
+            
+            image = [
+                {
+                    "mime_type" : file.type,
+                    "data" : bytes_data
+                }
+            ]
+            
+            return image
+    except:
+        raise FileNotFoundError("No file has been uploaded.")
+    
+
+
+
+    
